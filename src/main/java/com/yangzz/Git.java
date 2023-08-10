7a643009b80c30bc3e2e84cfdf580068183a6e85
yangzz <yangzz@163.com>
2023-08-10 20:18:13 +0800

    push commit

--- src/main/java/com/yangzz/Git.java
+++ src/main/java/com/yangzz/Git.java
@@ -3,5 +3,6 @@ package com.yangzz;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello,git!!!");
+        System.out.println("push test!!!");
     }
 }
